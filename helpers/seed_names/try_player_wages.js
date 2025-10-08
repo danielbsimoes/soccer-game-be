c8d15a40aea3bb5df5bbfa9d5cd897d6b2e68f2a
daniel <danielsimoesdbs@gmail.com>
2025-10-08 10:26:06 +0100

    add player wage testing script

---
+++ helpers/seed_names/try_player_wages.js
@@ -0,0 +1,32 @@
+import { generateValue } from "./generate_value.js";
+
+const rating = [1,2, 3,4, 5, 12, 24, 32, 45, 58, 64, 71, 72, 88, 96, 99, 100];
+const age = [16, 19, 22, 25, 26, 28, 30, 31, 33, 35, 37];
+const opts = {
+    BASE_VALUE: 100,
+    MIN_VALUE: 50,
+    MAX_VALUE: 30_000_000,
+    AGE_PEAK: 26,
+    RATING_FACTOR: 0.005,
+    LOWEST_RATING_BOOST_FACTOR: 7,
+    AGE_FACTOR: 0,
+    RANDOMNESS_FACTOR: 0.15
+};
+
+for (let i=0; i<rating.length; i++) {
+    console.log(`\n--- Rating: ${rating[i]} ---`);
+    for (let j=0; j<age.length; j++) {
+        const value = stringifyValue(generateValue(rating[i], age[j], opts));
+        console.log(`Age: ${age[j]}, Rating: ${rating[i]} => Value: ${value}`);
+    }
+}
+
+function stringifyValue(value) {
+    if (value >= 1_000_000) {
+        return (value / 1_000_000).toFixed(2).replace(/\.0$/, '') + 'M€/month';
+    } else if (value >= 1_000) {
+        return (value / 1_000).toFixed(2).replace(/\.0$/, '') + 'K€/month';
+    } else {
+        return value.toFixed(2) + '€/month';
+    }
+}
