34d7c9eb570e93ec491ce94481a5e93e66b46093
daniel <danielsimoesdbs@gmail.com>
2025-10-10 01:05:58 +0100

    fix --env-file path

--- helpers/seeds/create_all_seeds.sh
+++ helpers/seeds/create_all_seeds.sh
@@ -4,16 +4,16 @@ scriptsFolder=${rootFolder}/scripts
 echo "Creating all seeds..."
 
 echo "Generating names seed..."
-node --env-file=.env ${helperSeedsFolder}/names/import_names_json_file.js
+node --env-file=${rootFolder}/.env ${helperSeedsFolder}/names/import_names_json_file.js
 
 echo "Executing name seeds..."
-node --env-file=.env ${helperSeedsFolder}/execute_seed_files.js ${scriptsFolder}/04-seed-data-names.sql
+node --env-file=${rootFolder}/.env ${helperSeedsFolder}/execute_seed_files.js ${scriptsFolder}/04-seed-data-names.sql
 
 echo "Generating players seed..."
-node --env-file=.env ${helperSeedsFolder}/player/seed_player_data.js
+node --env-file=${rootFolder}/.env ${helperSeedsFolder}/player/seed_player_data.js
 
 echo "Generating staff seed..."
-node --env-file=.env ${helperSeedsFolder}/staff/seed_staff_data.js
+node --env-file=${rootFolder}/.env ${helperSeedsFolder}/staff/seed_staff_data.js
 
 echo "Executing all other sql seeds..."
-node --env-file=.env ${helperSeedsFolder}/execute_seed_files.js ${scriptsFolder}/05-seed-data-players.sql ${scriptsFolder}/06-seed-data-staff.sql
+node --env-file=${rootFolder}/.env ${helperSeedsFolder}/execute_seed_files.js ${scriptsFolder}/05-seed-data-players.sql ${scriptsFolder}/06-seed-data-staff.sql
